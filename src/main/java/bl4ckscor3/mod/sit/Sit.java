0bb680f191a0056ad5d05c2b8ca9833008fe7876
bl4ckscor3 <donalddani9612@gmx.de>
2019-02-10 17:38:26 +0100

    update to 19w06a, doesn't quite work yet

--- src/main/java/bl4ckscor3/mod/sit/Sit.java
+++ src/main/java/bl4ckscor3/mod/sit/Sit.java
@@ -1,37 +1,39 @@
 package bl4ckscor3.mod.sit;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.entity.FabricEntityTypeBuilder;
-import net.fabricmc.fabric.events.PlayerInteractionEvent;
+import net.fabricmc.fabric.api.entity.FabricEntityTypeBuilder;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.block.StairsBlock;
 import net.minecraft.block.enums.BlockHalf;
 import net.minecraft.block.enums.SlabType;
+import net.minecraft.entity.EntityCategory;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.registry.Registry;
 
 public class Sit implements ModInitializer
 {
-	public static final EntityType<EntitySit> ENTITY_SIT = Registry.register(Registry.ENTITY_TYPE, "sit:entity_sit", FabricEntityTypeBuilder.create(EntitySit.class, EntitySit::new).build());
+	public static final EntityType<EntitySit> ENTITY_SIT = Registry.register(Registry.ENTITY_TYPE, "sit:entity_sit", FabricEntityTypeBuilder.create(EntityCategory.MISC, EntitySit::new).build());
 
 	@Override
 	public void onInitialize()
 	{
-		PlayerInteractionEvent.INTERACT_BLOCK.register((player, world, hand, pos, direction, v, v1, v2) -> {
-			BlockState s = world.getBlockState(pos);
-			Block b = world.getBlockState(pos).getBlock();
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+			BlockState s = world.getBlockState(hitResult.getBlockPos());
+			Block b = world.getBlockState(hitResult.getBlockPos()).getBlock();
 
-			if((b instanceof SlabBlock || b instanceof StairsBlock) && !EntitySit.OCCUPIED.containsKey(pos) && player.getStackInHand(hand).isEmpty())
+			if((b instanceof SlabBlock || b instanceof StairsBlock) && !EntitySit.OCCUPIED.containsKey(hitResult.getBlockPos()) && player.getStackInHand(hand).isEmpty())
 			{
 				if(b instanceof SlabBlock && (!s.getProperties().contains(SlabBlock.TYPE) || s.get(SlabBlock.TYPE) != SlabType.BOTTOM))
 					return ActionResult.PASS;
 				else if(b instanceof StairsBlock && (!s.getProperties().contains(StairsBlock.HALF) || s.get(StairsBlock.HALF) != BlockHalf.BOTTOM))
 					return ActionResult.PASS;
 
-				EntitySit sit = new EntitySit(world, pos);
+				EntitySit sit = new EntitySit(world, hitResult.getBlockPos());
 
 				world.spawnEntity(sit);
 				player.startRiding(sit);
@@ -41,7 +43,7 @@ public class Sit implements ModInitializer
 			return ActionResult.PASS;
 		});
 
-		PlayerInteractionEvent.ATTACK_BLOCK.register((player, world, hand, pos, direction) -> {
+		AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
 			if(EntitySit.OCCUPIED.containsKey(pos))
 				EntitySit.OCCUPIED.get(pos).kill();
 
