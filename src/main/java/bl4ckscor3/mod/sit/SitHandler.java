5980aae6d535cf63f31a393833d4b28072e65360
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-12-24 12:58:24 +0100

    add config option to set how far away a player can be to be able to sit

--- src/main/java/bl4ckscor3/mod/sit/SitHandler.java
+++ src/main/java/bl4ckscor3/mod/sit/SitHandler.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.state.properties.Half;
 import net.minecraft.state.properties.SlabType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityMountEvent;
@@ -18,7 +19,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
+@EventBusSubscriber(modid=Sit.MODID)
 public class SitHandler
 {
 	@SubscribeEvent
@@ -32,7 +33,7 @@ public class SitHandler
 			Block block = world.getBlockState(pos).getBlock();
 			PlayerEntity player = event.getPlayer();
 
-			if((block instanceof SlabBlock || block instanceof StairsBlock || isModBlock(block)) && !SitUtil.isOccupied(world, pos) && player.getHeldItemMainhand().isEmpty() && world.getBlockState(pos.up()).isAir(world, pos.up()))
+			if(isValidBlock(world, pos, block) && isPlayerInRange(player, pos) && !SitUtil.isOccupied(world, pos) && player.getHeldItemMainhand().isEmpty() && world.getBlockState(pos.up()).isAir(world, pos.up()))
 			{
 				if(block instanceof SlabBlock && (!state.has(SlabBlock.TYPE) || state.get(SlabBlock.TYPE) != SlabType.BOTTOM))
 					return;
@@ -75,6 +76,18 @@ public class SitHandler
 	}
 
 	/**
+	 * Returns whether or not the given block can be sat on
+	 * @param world The world to check in
+	 * @param pos The position to check at
+	 * @param block The block to check
+	 * @return true if the given block can be sat one, false otherwhise
+	 */
+	private static boolean isValidBlock(World world, BlockPos pos, Block block)
+	{
+		return (block instanceof SlabBlock || block instanceof StairsBlock || isModBlock(block));
+	}
+
+	/**
 	 * Checks wether the given block is a specific block from a mod. Used to support
 	 * stairs/slabs from other mods that don't work with Sit by default.
 	 * @param block The block to check
@@ -87,5 +100,27 @@ public class SitHandler
 		else*/ if(ModList.get().isLoaded("snowvariants") && block instanceof trikzon.snowvariants.blocks.SnowSlab)
 			return true;
 		else return false;
+	}
+
+	/**
+	 * Returns whether or not the player is close enough to the block to be able to sit on it
+	 * @param player The player
+	 * @param pos The position of the block to sit on
+	 * @return true if the player is close enough, false otherwhise
+	 */
+	private static boolean isPlayerInRange(PlayerEntity player, BlockPos pos)
+	{
+		BlockPos playerPos = player.getPosition();
+		int blockReachDistance = Configuration.CONFIG.blockReachDistance.get();
+
+		if(blockReachDistance == 0) //player has to stand on top of the block
+			return playerPos.getY() - pos.getY() <= 1 && playerPos.getX() - pos.getX() == 0 && playerPos.getZ() - pos.getZ() == 0;
+
+		pos = pos.add(0.5D, 0.5D, 0.5D);
+
+		AxisAlignedBB range = new AxisAlignedBB(pos.getX() + blockReachDistance, pos.getY() + blockReachDistance, pos.getZ() + blockReachDistance, pos.getX() - blockReachDistance, pos.getY() - blockReachDistance, pos.getZ() - blockReachDistance);
+
+		playerPos = playerPos.add(0.5D, 0.5D, 0.5D);
+		return range.minX <= playerPos.getX() && range.minY <= playerPos.getY() && range.minZ <= playerPos.getZ() && range.maxX >= playerPos.getX() && range.maxY >= playerPos.getY() && range.maxZ >= playerPos.getZ();
 	}
 }
