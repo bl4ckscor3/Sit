bcb71a82ed28c136357639127e561a45c228225c
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-03-29 17:29:35 +0200

    fix #22

--- src/main/java/bl4ckscor3/mod/sit/SitEntity.java
+++ src/main/java/bl4ckscor3/mod/sit/SitEntity.java
@@ -1,6 +1,7 @@
 package bl4ckscor3.mod.sit;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
@@ -9,6 +10,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.SupportType;
 import net.minecraft.world.phys.Vec3;
 
 public class SitEntity extends Entity
@@ -34,8 +36,15 @@ public class SitEntity extends Entity
 
 			if(pos != null)
 			{
+				Vec3 resetPosition = new Vec3(pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D);
+				BlockPos belowResetPos = new BlockPos(resetPosition.x, resetPosition.y - 1, resetPosition.z);
+
 				discard();
-				return new Vec3(pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D);
+
+				if(!player.level.getBlockState(belowResetPos).isFaceSturdy(level, belowResetPos, Direction.UP, SupportType.FULL))
+					return new Vec3(resetPosition.x, resetPosition.y + 1, resetPosition.z);
+				else
+					return resetPosition;
 			}
 		}
 
