c37a7189ab278edd36385986077aca75782075b6
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-11-20 16:09:08 +0100

    fix compatibility with NeoForge 20.2.59-beta

--- src/main/java/bl4ckscor3/mod/sit/Sit.java
+++ src/main/java/bl4ckscor3/mod/sit/Sit.java
@@ -1,20 +1,20 @@
 package bl4ckscor3.mod.sit;
 
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.ForgeRegistries;
-import net.neoforged.neoforge.registries.RegistryObject;
 
 @Mod(Sit.MODID)
 public class Sit {
 	public static final String MODID = "sit";
-	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MODID);
-	public static final RegistryObject<EntityType<SitEntity>> SIT_ENTITY_TYPE = ENTITY_TYPES.register("entity_sit", () -> EntityType.Builder.<SitEntity>of(SitEntity::new, MobCategory.MISC)
+	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(Registries.ENTITY_TYPE, MODID);
+	public static final DeferredHolder<EntityType<?>, EntityType<SitEntity>> SIT_ENTITY_TYPE = ENTITY_TYPES.register("entity_sit", () -> EntityType.Builder.<SitEntity>of(SitEntity::new, MobCategory.MISC)
 	//@formatter:off
 			.setTrackingRange(256)
 			.setUpdateInterval(20)
