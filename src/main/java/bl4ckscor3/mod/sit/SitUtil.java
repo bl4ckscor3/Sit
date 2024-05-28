e18acf61741b54b7533c514e93e860a676bf2956
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-05-28 11:22:12 +0200

    fix #32

--- src/main/java/bl4ckscor3/mod/sit/SitUtil.java
+++ src/main/java/bl4ckscor3/mod/sit/SitUtil.java
@@ -10,6 +10,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 /**
  * Use this class to manage sit entities correctly
@@ -18,7 +19,7 @@ public class SitUtil {
 	/**
 	 * <dimension type id, <position, <entity, previous player position>>> This map only gets populated on server side.
 	 */
-	private static final Map<ResourceLocation, Map<BlockPos, Pair<SitEntity, BlockPos>>> OCCUPIED = new HashMap<>();
+	private static final Map<ResourceLocation, Map<BlockPos, Pair<SitEntity, Vec3>>> OCCUPIED = new HashMap<>();
 
 	private SitUtil() {}
 
@@ -33,7 +34,7 @@ public class SitUtil {
 	public static void sitDown(Player player, Level level, BlockPos pos, double yOffset) {
 		SitEntity sit = new SitEntity(level, pos, yOffset);
 
-		if (SitUtil.addSitEntity(level, pos, sit, player.blockPosition())) {
+		if (SitUtil.addSitEntity(level, pos, sit, player.position())) {
 			level.addFreshEntity(sit);
 			player.startRiding(sit);
 		}
@@ -49,7 +50,7 @@ public class SitUtil {
 	 *            dismounting
 	 * @return true if the entity was added, false otherwhise. This is always false on the client.
 	 */
-	public static boolean addSitEntity(Level level, BlockPos blockPos, SitEntity entity, BlockPos playerPos) {
+	public static boolean addSitEntity(Level level, BlockPos blockPos, SitEntity entity, Vec3 playerPos) {
 		if (!level.isClientSide) {
 			ResourceLocation id = getDimensionTypeId(level);
 
@@ -107,12 +108,12 @@ public class SitUtil {
 	 * @return The position the player was at before he sat down, null if the player is not sitting. This is always null on the
 	 *         client.
 	 */
-	public static BlockPos getPreviousPlayerPosition(Player player, SitEntity sitEntity) {
+	public static Vec3 getPreviousPlayerPosition(Player player, SitEntity sitEntity) {
 		if (!player.level().isClientSide) {
 			ResourceLocation id = getDimensionTypeId(player.level());
 
 			if (OCCUPIED.containsKey(id)) {
-				for (Pair<SitEntity, BlockPos> pair : OCCUPIED.get(id).values()) {
+				for (Pair<SitEntity, Vec3> pair : OCCUPIED.get(id).values()) {
 					if (pair.getLeft() == sitEntity)
 						return pair.getRight();
 				}
@@ -143,8 +144,8 @@ public class SitUtil {
 	 * @return true if the given player is sitting anywhere, false otherwhise
 	 */
 	public static boolean isPlayerSitting(Player player) {
-		for (Entry<ResourceLocation, Map<BlockPos, Pair<SitEntity, BlockPos>>> entry : OCCUPIED.entrySet()) {
-			for (Pair<SitEntity, BlockPos> pair : entry.getValue().values()) {
+		for (Entry<ResourceLocation, Map<BlockPos, Pair<SitEntity, Vec3>>> entry : OCCUPIED.entrySet()) {
+			for (Pair<SitEntity, Vec3> pair : entry.getValue().values()) {
 				if (pair.getLeft().hasPassenger(player))
 					return true;
 			}
