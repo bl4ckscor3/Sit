ac228bd63e87792039c71fe7a11cd4b12932ea3f
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-06-14 10:35:43 +0200

    update to 1.21

--- src/main/java/bl4ckscor3/mod/sit/SitEntity.java
+++ src/main/java/bl4ckscor3/mod/sit/SitEntity.java
@@ -7,6 +7,7 @@ import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -64,7 +65,7 @@ public class SitEntity extends Entity {
 	protected void addAdditionalSaveData(CompoundTag tag) {}
 
 	@Override
-	public Packet<ClientGamePacketListener> getAddEntityPacket() {
-		return new ClientboundAddEntityPacket(this);
+	public Packet<ClientGamePacketListener> getAddEntityPacket(ServerEntity serverEntity) {
+		return new ClientboundAddEntityPacket(this, serverEntity);
 	}
 }
